a938c30b0f46eb6674c786a86ab85cb7de36b2c3
YongUnChoi <daypekr@gmail.com>
2016-02-11 18:02:35 +0100

    bugfix on previous commit
    wrong template file had been used in the previous commit

--- wp-content/themes/jv-allinone-child/woocommerce/single-product/add-to-cart/variable.php
+++ wp-content/themes/jv-allinone-child/woocommerce/single-product/add-to-cart/variable.php
@@ -1,14 +1,27 @@
 <?php
+
 /**
+
  * Variable product add to cart
+
  *
+
  * @author  WooThemes
+
  * @package WooCommerce/Templates
+
  * @version 2.4.0
+
  */
+
+
+
 if ( ! defined( 'ABSPATH' ) ) {
+
 	exit;
+
 }
+
 function user_has_role( $role, $user_id = null ) {
 
     if ( is_numeric( $user_id ) )
@@ -25,70 +38,241 @@ function HideCaseOf6($string) {
   return strpos($string, 'caseof6') === false;
 }
 
-global $product;
+global $product, $post;
 
-$attribute_keys = array_keys( $attributes );
+?>
 
-do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-<form class="variations_form cart" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->id ); ?>" data-product_variations="<?php echo esc_attr( json_encode( $available_variations ) ) ?>">
-	<?php do_action( 'woocommerce_before_variations_form' ); ?>
 
-	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
-		<p class="stock out-of-stock"><?php _e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
-	<?php else : ?>
-		<table class="variations" cellspacing="0">
-			<tbody>
-				<?php
-				 foreach ( $attributes as $attribute_name => $options ) : 
-				 	//var_dump($options);
+<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+
+
+
+<form class="variations_form cart borderBottom" method="post" enctype='multipart/form-data' data-product_id="<?php echo esc_attr($post->ID); ?>" data-product_variations="<?php echo esc_attr( json_encode( $available_variations ) ) ?>">
+
+	<?php if ( ! empty( $available_variations ) ) : ?>
+
+		<div class="variations">
+
+
+
+				<?php $loop = 0; foreach ( $attributes as $name => $options ) : 
 				if(user_has_role('whole_saler') || user_has_role('distributor')){
 					$options = array_filter($options, 'HideCaseOf6');
-
 				}
-				?>
-					<tr>
-						<td class="label"><label for="<?php echo sanitize_title( $attribute_name ); ?>"><?php echo wc_attribute_label( $attribute_name ); ?></label></td>
-						<td class="value">
+				$loop++; ?>
+
+					<div class="item">
+
+						<label for="<?php echo sanitize_title($name); ?>"><?php echo wc_attribute_label( $name ); ?></label>
+
+						<div class="css-select"><select id="<?php echo esc_attr( sanitize_title( $name ) ); ?>" name="attribute_<?php echo sanitize_title( $name ); ?>">
+
+							<option value=""><?php echo __( 'Choose an option', 'jv_allinone' ) ?>&hellip;</option>
+
 							<?php
-								$selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ? wc_clean( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) : $product->get_variation_default_attribute( $attribute_name );
-								wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected ) );
-								echo end( $attribute_keys ) === $attribute_name ? '<a class="reset_variations" href="#">' . __( 'Clear selection', 'woocommerce' ) . '</a>' : '';
+
+								if ( is_array( $options ) ) {
+
+
+
+									
+
+									
+
+									if ( isset( $_POST[ 'attribute_' . sanitize_title( $name ) ] ) ) {
+
+										$selected_value = $_POST[ 'attribute_' . sanitize_title( $name ) ];
+
+									}
+
+									else{
+
+											if( isset( $_GET[ 'attribute_' . sanitize_title( $name ) ] ) ){
+
+												$selected_value = $_GET[ 'attribute_' . sanitize_title( $name ) ];
+
+											}
+
+											else{
+
+													if( isset( $selected_attributes[ sanitize_title( $name ) ] )){
+
+															$selected_value = $selected_attributes[ sanitize_title( $name ) ];
+
+													}
+
+													else{
+
+															$selected_value = '';
+
+													}
+
+											}
+
+									}
+
+
+
+									// Get terms if this is a taxonomy - ordered
+
+									if ( taxonomy_exists( sanitize_title( $name ) ) ) {
+
+
+
+										$orderby = wc_attribute_orderby( sanitize_title( $name ) );
+
+
+
+										switch ( $orderby ) {
+
+											case 'name' :
+
+												$args = array( 'orderby' => 'name', 'hide_empty' => false, 'menu_order' => false );
+
+											break;
+
+											case 'id' :
+
+												$args = array( 'orderby' => 'id', 'order' => 'ASC', 'menu_order' => false, 'hide_empty' => false );
+
+											break;
+
+											case 'menu_order' :
+
+												$args = array( 'menu_order' => 'ASC', 'hide_empty' => false );
+
+											break;
+
+										}
+
+
+
+										$terms = get_terms( sanitize_title( $name ), $args );
+
+
+
+										foreach ( $terms as $term ) {
+
+											if ( ! in_array( $term->slug, $options ) )
+
+												continue;
+
+
+
+											echo '<option value="' . esc_attr( $term->slug ) . '" ' . selected( sanitize_title( $selected_value ), sanitize_title( $term->slug ), false ) . '>' . apply_filters( 'woocommerce_variation_option_name', $term->name ) . '</option>';
+
+										}
+
+									} else {
+
+
+
+										foreach ( $options as $option ) {
+
+											echo '<option value="' . esc_attr( sanitize_title( $option ) ) . '" ' . selected( sanitize_title( $selected_value ), sanitize_title( $option ), false ) . '>' . esc_html( apply_filters( 'woocommerce_variation_option_name', $option ) ) . '</option>';
+
+										}
+
+
+
+									}
+
+								}
+
 							?>
-						</td>
-					</tr>
+
+						</select> </div>
+
+                        <div class="bottom-border"></div>
+
+					</div>
+
+                    
+
+                    
+
+                    <?php
+
+							if ( sizeof( $attributes ) == $loop )
+
+								echo '<a class="reset_variations" href="#reset">' . __( 'Clear selection', 'jv_allinone' ) . '</a>';
+
+						?>
+
 		        <?php endforeach;?>
-			</tbody>
-		</table>
+
+
+
+		</div>
+
+
 
 		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
+
+
 		<div class="single_variation_wrap" style="display:none;">
-			<?php
-				/**
-				 * woocommerce_before_single_variation Hook
-				 */
-				do_action( 'woocommerce_before_single_variation' );
 
-				/**
-				 * woocommerce_single_variation hook. Used to output the cart button and placeholder for variation data.
-				 * @since 2.4.0
-				 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
-				 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
-				 */
-				do_action( 'woocommerce_single_variation' );
+			<?php do_action( 'woocommerce_before_single_variation' ); ?>
 
-				/**
-				 * woocommerce_after_single_variation Hook
-				 */
-				do_action( 'woocommerce_after_single_variation' );
-			?>
+
+
+			<div class="single_variation"></div>
+
+
+
+			<div class="variations_button">
+
+				<?php woocommerce_quantity_input(); ?>
+
+				<button type="submit" class="single_add_to_cart_button btn btn-primary alt"><i class="icon-cart42"></i> <?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+
+			</div>
+
+		   	<?php if(shortcode_exists( 'yith_wcwl_add_to_wishlist' )){?><div class="item-btn"> <?php  echo do_shortcode("[yith_wcwl_add_to_wishlist]"); ?></div><?php } ?> 
+
+		    <?php if(shortcode_exists( 'yith_compare_button' )){?><div class="item-btn"> <?php  echo do_shortcode("[yith_compare_button]"); ?></div><?php } ?> 
+
+
+
+
+
+			
+
+			<input type="hidden" name="add-to-cart" value="<?php echo esc_attr($product->id); ?>" />
+
+			<input type="hidden" name="product_id" value="<?php echo esc_attr( $post->ID ); ?>" />
+
+			<input type="hidden" name="variation_id" value="" />
+
+
+
+			<?php do_action( 'woocommerce_after_single_variation' ); ?>
+
 		</div>
 
+
+
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+
+
+
+	<?php else : ?>
+
+
+
+		<p class="stock out-of-stock"><?php esc_attr_e( 'This product is currently out of stock and unavailable.', 'jv_allinone' ); ?></p>
+
+
+
 	<?php endif; ?>
 
-	<?php do_action( 'woocommerce_after_variations_form' ); ?>
+
+
 </form>
 
-<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+
+
+<?php do_action( 'woocommerce_after_add_to_cart_form' );
+
